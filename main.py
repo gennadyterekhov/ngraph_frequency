b2ffc85496a7045ee505ff0e2d3fea086eaba243
Gennady Terekhov <ter.ge@yandex.ru>
2024-05-29 20:00:15 +0400

    refact

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import json
-
-
-
 import csv
+
 
 def csv_to_dict(filepath, delimiter='\t', quotechar='"'):
     """
@@ -19,47 +17,28 @@ def csv_to_dict(filepath, delimiter='\t', quotechar='"'):
 
     data = []
     with open(filepath, 'r', newline='') as csvfile:
-        reader = csv.DictReader(csvfile, delimiter=delimiter, quotechar=quotechar)
+        reader = csv.DictReader(
+            csvfile, delimiter=delimiter, quotechar=quotechar)
         for row in reader:
             data.append(row)
     return data
 
 
-
-
-def getDictionaryAsDict()->dict:
+def getDictionaryAsDict() -> dict:
     filename = 'freqrnc2011.csv'
     return csv_to_dict(filename)
 
-def fillEntriesMap(dct:dict):
+
+def fillEntriesMap(dct: dict):
     for wordData in dct:
-        # print(wordData)
-        # exit()
         processWord(wordData['Lemma'], float(wordData['Freq(ipm)']))
 
-# def fillFreqsMap():
-    # for key, value in enumerate(digraphToEntriesMap):
-    #     digraphToFreqMap[key] = value / totalDigraphs
 
-def output():
-    handle = open('res.json', 'w')
-    # handle.write(json.dumps(digraphToEntriesMap, ensure_ascii=False))
-    handle.write(json.dumps(freqList, ensure_ascii=False))
+def processWord(word: str, freq: float):
+    digs = getDigraphsFromWord(word)
+    for d in digs:
+        addToMap(d, freq)
 
-    handle.close()
-
-
-def turnIntoList():
-    for key, value in digraphToEntriesMap.items():
-        freqList.append(
-            {"digraph": key, "freq": value}            
-        )
-
-def sort():
-    freqList.sort(key=sortKey, reverse=True)
-
-def sortKey(ob):
-    return ob['freq']
 
 def getDigraphsFromWord(word: str) -> list:
     l = len(word)
@@ -81,30 +60,38 @@ def addToMap(key: str, newVal: float):
     digraphToEntriesMap[key] += newVal
 
 
-def processWord(word: str, freq: float):
-    global totalDigraphs
-    digs = getDigraphsFromWord(word)
-    for d in digs:
-        addToMap(d, freq)
-        totalDigraphs += freq
+def turnIntoList():
+    for key, value in digraphToEntriesMap.items():
+        freqList.append(
+            {"digraph": key, "freq": value}
+        )
+
+
+def sort():
+    freqList.sort(key=sortKey, reverse=True)
+
+
+def sortKey(ob):
+    return ob['freq']
+
+
+def output():
+    handle = open('res.json', 'w')
+    handle.write(json.dumps(freqList, ensure_ascii=False))
+    handle.close()
 
 
 digraphToEntriesMap = {}
-digraphToFreqMap = {}
 freqList = []
-totalDigraphs = 0
-
 
 
 def main():
     dct = getDictionaryAsDict()
-    # print(dct)
     fillEntriesMap(dct)
-    # useless
-    # fillFreqsMap()
     turnIntoList()
     sort()
     output()
-    
+
+
 if __name__ == '__main__':
     main()
